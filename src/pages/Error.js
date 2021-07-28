d1abe0c6c6674a22c34138babd22104fc44bfcde
Kevin Mazats <k.mazats@gmail.com>
2021-07-28 16:23:24 +0200

    responsive 404

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -5,14 +5,16 @@ import "../styles/Error.css";
 class Error extends React.Component {
 	render() {
 		return (
-			<div className="error">
-				<h1 className="error__title">404</h1>
-				<p className="error__message">
-					Oups! La page que vous demandez n'existe pas.
-				</p>
-				<Link to="/" className="error__link">
-					Retourner sur la page d’accueil
-				</Link>
+			<div className="layout">
+				<div className="error">
+					<h1 className="error__title">404</h1>
+					<p className="error__message">
+						Oups! La page que vous demandez n'existe pas.
+					</p>
+					<Link to="/" className="error__link">
+						Retourner sur la page d’accueil
+					</Link>
+				</div>
 			</div>
 		);
 	}
