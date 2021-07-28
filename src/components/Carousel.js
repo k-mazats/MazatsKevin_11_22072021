f24164ec87909d10235978b2eebb53cc5226028d
Kevin Mazats <k.mazats@gmail.com>
2021-07-28 15:35:34 +0200

    tabIndex + keyboard events

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -6,29 +6,43 @@ class Carousel extends React.Component {
 	state = {
 		currentPicture: 0,
 	};
-	nextPicture = () =>{
-    this.state.currentPicture === this.props.pictures.length - 1 ?
-    this.setState({ currentPicture: 0 })
-    :
-    this.setState({currentPicture: this.state.currentPicture + 1})
-  }
-	previousPicture = () =>{
-    this.state.currentPicture === 0
+	nextPicture = () => {
+		this.state.currentPicture === this.props.pictures.length - 1
+			? this.setState({ currentPicture: 0 })
+			: this.setState({ currentPicture: this.state.currentPicture + 1 });
+	};
+	previousPicture = () => {
+		this.state.currentPicture === 0
 			? this.setState({ currentPicture: this.props.pictures.length - 1 })
 			: this.setState({ currentPicture: this.state.currentPicture - 1 });
-  }
+	};
+	handleKeyPress = (e, callback) => {
+		if (e.charCode === 13) {
+			e.preventDefault();
+			e.stopPropagation();
+			callback();
+		}
+	};
 	render() {
 		return (
 			<div className="carousel">
 				<div
 					className="carousel__controls carousel__controls--previous"
 					onClick={this.previousPicture}
+					onKeyPress={(event) => {
+						this.handleKeyPress(event, this.previousPicture);
+					}}
+					tabIndex="0"
 				>
 					<i className="fas fa-chevron-left"></i>
 				</div>
 				<div
 					className="carousel__controls carousel__controls--next"
 					onClick={this.nextPicture}
+					onKeyPress={(event) => {
+						this.handleKeyPress(event, this.nextPicture);
+					}}
+					tabIndex="0"
 				>
 					<i className="fas fa-chevron-right"></i>
 				</div>
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -9,19 +9,35 @@ class Dropdown extends React.Component {
 	toggleDropdown = () => {
 		this.setState({ visible: !this.state.visible });
 	};
-	fillContent = (content) =>{
-if (Array.isArray(content)) {
-	return (<ul>{content.map((equipment,index) => (
-		<li key={`equipment${index}`}>{equipment}</li>
-	))}</ul>)
-}else{
-return <p>{content}</p>;
-}
+	fillContent = (content) => {
+		if (Array.isArray(content)) {
+			return (
+				<ul>
+					{content.map((equipment, index) => (
+						<li key={`equipment${index}`}>{equipment}</li>
+					))}
+				</ul>
+			);
+		} else {
+			return <p>{content}</p>;
+		}
+	};
+	handleKeyPress = (e) => {
+		if (e.charCode === 13) {
+			e.preventDefault();
+			e.stopPropagation();
+			this.toggleDropdown()
+		}
 	}
 	render() {
 		return (
 			<div className="dropdown">
-				<div className="dropdown__toggle" onClick={this.toggleDropdown}>
+				<div
+					className="dropdown__toggle"
+					onClick={this.toggleDropdown}
+					onKeyPress={this.handleKeyPress}
+					tabIndex="0"
+				>
 					<div className="dropdown__toggle-title">{this.props.title}</div>
 					<div className="dropdown__toggle-icon">
 						{this.state.visible ? (
@@ -34,7 +50,6 @@ return <p>{content}</p>;
 				{this.state.visible ? (
 					<div className="dropdown__content">
 						{this.fillContent(this.props.content)}
-						
 					</div>
 				) : null}
 			</div>
